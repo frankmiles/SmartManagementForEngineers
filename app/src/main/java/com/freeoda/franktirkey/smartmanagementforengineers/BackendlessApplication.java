1a2befc24c0cde16e432c40c0bad98a2a6290201
FrankMiles <38081214+frankmiles@users.noreply.github.com>
2020-01-02 17:41:44 +0530

    Update BackendlessApplication.java

--- app/src/main/java/com/freeoda/franktirkey/smartmanagementforengineers/BackendlessApplication.java
+++ app/src/main/java/com/freeoda/franktirkey/smartmanagementforengineers/BackendlessApplication.java
@@ -13,7 +13,7 @@ public class BackendlessApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        Backendless.initApp( this,"A32C8534-709A-4E2C-829B-0614A16E5DF3", "D5A68302-4647-45AC-82F2-33906138BFB0");
+        Backendless.initApp( this,"", "");
     }
 
     public void distroyBackendlessUser(){
