a61dd44c4faedeb3133182c1a7adbf4f968cb31a
FrankMiles <38081214+frankmiles@users.noreply.github.com>
2020-01-21 03:29:10 +0530

    STABLE fixed to download and parse xml Version to 2.0.8

--- app/src/main/java/com/freeoda/franktirkey/smartmanagementforengineers/MainActivity.java
+++ app/src/main/java/com/freeoda/franktirkey/smartmanagementforengineers/MainActivity.java
@@ -1,9 +1,13 @@
 package com.freeoda.franktirkey.smartmanagementforengineers;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 import androidx.viewpager.widget.PagerAdapter;
 import androidx.viewpager.widget.ViewPager;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.WindowManager;
 import android.widget.Toast;
@@ -17,6 +21,12 @@ public class MainActivity extends AppCompatActivity {
     TabLayout tabLayout;
     TabItem tabResent,tabHome,tabPlan;
     ViewPager viewPager;
+
+    private static final int REQUEST_EXTERNAL_STORAGE = 1;
+    private static String[] PERMISSIONS_STORAGE = {
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -55,5 +65,19 @@ public class MainActivity extends AppCompatActivity {
 
         Toast.makeText(MainActivity.this,BackendlessApplication.backendlessUser.getProperty("email").toString(),Toast.LENGTH_LONG).show();
 
+        verifyStoragePermissions(MainActivity.this);
+    }
+
+    public static void verifyStoragePermissions(Activity activity) {
+        // Check if we have write permission
+        int permission = ActivityCompat.checkSelfPermission(activity, Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (permission != PackageManager.PERMISSION_GRANTED) {
+            // We don't have permission so prompt the user
+            ActivityCompat.requestPermissions(
+                    activity,
+                    PERMISSIONS_STORAGE,
+                    REQUEST_EXTERNAL_STORAGE
+            );
+        }
     }
 }
